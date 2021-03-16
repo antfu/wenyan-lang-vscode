c93845d2488ecf3c253fecc7799f10fb188f1b4a
XiLaiTL <1272720210@qq.com>
2021-03-16 09:23:39 +0800

    Update dynamicSnippets.ts

--- src/editor/dynamicSnippets.ts
+++ src/editor/dynamicSnippets.ts
@@ -2,6 +2,7 @@ import { window, workspace, TextDocumentChangeEvent, Range, Position, Selection,
 import { ExtensionModule } from '../module'
 import DynamicSnippets from '../../snippets/dynamic.json'
 import { LANG_ID } from '../meta'
+import { num2hanzi } from './converts/hanzi2num'
 
 const BODY_PARAMETER_REGEX = /\$\d+/g
 
@@ -61,79 +62,20 @@ function onTextChanged(e: TextDocumentChangeEvent) {
     }
   }
 }
-function translateToChinese(num: string): string {
-  let i = Number(num)
-  let j: number = num.length
-  let re = ''
-  let l = 0
-  for (j; j > 0; j = j - 1) {
-    const k = i % 10
-    switch (k) {
-      case 1 : re = `一${re}`
-        break
-      case 2 : re = `二${re}`
-        break
-      case 3 : re = `三${re}`
-        break
-      case 4 : re = `四${re}`
-        break
-      case 5 : re = `五${re}`
-        break
-      case 6 : re = `六${re}`
-        break
-      case 7 : re = `七${re}`
-        break
-      case 8 : re = `八${re}`
-        break
-      case 9 : re = `九${re}`
-        break
-      case 0 : re = `零${re}`
-        break
-    }
-    i = i / 10
-    i = Math.floor(i)
-    if (i > 0) {
-      l += 1
-      switch (l) {
-        case 1 : re = `十${re}`
-          break
-        case 2 : re = `百${re}`
-          break
-        case 3 : re = `千${re}`
-          break
-        case 4 : re = `萬${re}`
-          break
-        case 5 : re = `十萬${re}`
-          break
-        case 6 : re = `百萬${re}`
-          break
-        case 7 : re = `千萬${re}`
-          break
-        case 8 : re = `億${re}`
-          break
-        case 9 : re = `十億${re}`
-          break
-        case 10 : re = `百億${re}`
-          break
-        case 11 : re = `千億${re}`
-          break
-      }
-    }
-  }
-  return re
-}
 
 function provideCompletionItems(document: TextDocument, position: Position, token: CancellationToken, context: CompletionContext) {
   const line = document.lineAt(position)
   const lineText = line.text.substring(0, position.character)
   let lenth = lineText.length
   for (lenth; lenth >= 0; lenth -= 1) {
-    if (lineText[lenth - 1] !== '0' && lineText[lenth - 1] !== '1' && lineText[lenth - 1] !== '2' && lineText[lenth - 1] !== '3' && lineText[lenth - 1] !== '4' && lineText[lenth - 1] !== '5' && lineText[lenth - 1] !== '6' && lineText[lenth - 1] !== '7' && lineText[lenth - 1] !== '8' && lineText[lenth - 1] !== '9')
+    if (lineText[lenth - 1] !== '.' && lineText[lenth - 1] !== '0' && lineText[lenth - 1] !== '1' && lineText[lenth - 1] !== '2' && lineText[lenth - 1] !== '3' && lineText[lenth - 1] !== '4' && lineText[lenth - 1] !== '5' && lineText[lenth - 1] !== '6' && lineText[lenth - 1] !== '7' && lineText[lenth - 1] !== '8' && lineText[lenth - 1] !== '9')
       break
     else if (lineText[lenth - 1] === ' ')
       break
   }
+  console.log(lineText)
   const letter = line.text.substring(lenth, position.character)
+  console.log(letter)
   const de = [letter]
   return de.map((dee) => {
     return new CompletionItem(dee, CompletionItemKind.Text)
@@ -142,7 +84,8 @@ function provideCompletionItems(document: TextDocument, position: Position, toke
 
 function resolveCompletionItem(item: any, token: CancellationToken) {
   const letter = item.label
-  const numm = translateToChinese(letter)
+  const i = Number(letter)
+  const numm = num2hanzi(i)
   item = { label: numm, kind: 0 }
   return item
 }
